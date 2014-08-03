3998546d04e53034c9ed502f56d2c3e1f6ebc278
Crowifick <minecraft4455@live.com>
2014-08-03 16:51:26 -0700

    Added Clorite tools recipe items. Added names.(Working on Textures and content now!)

--- main/java/net/crowifick/atlasrise/items/ARItems.java
+++ main/java/net/crowifick/atlasrise/items/ARItems.java
@@ -5,6 +5,7 @@ import net.crowifick.atlasrise.utils.items.*;
 import net.crowifick.atlasrise.utils.Reference;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemArmor;
 import net.minecraftforge.common.util.EnumHelper;
 
 /**
@@ -18,8 +19,15 @@ import net.minecraftforge.common.util.EnumHelper;
 public class ARItems {
 
     public static Item.ToolMaterial clorite = EnumHelper.addToolMaterial("Clorite", 4, 500, 10.0F, 4.0F, 15);
+    public static ItemArmor.ArmorMaterial cloriteArmor = EnumHelper.addArmorMaterial("Clorite", 43, new int[]{5, 10, 8, 6}, 12);
 
     public static ARItem cloriteIngot = new ARItem("CloriteIngot", 64, CreativeTabs.tabMaterials);
+    public static ARItem cloritePlate = new ARItem("CloritePlate", 64, CreativeTabs.tabMaterials);
+    public static ARItem cloritePickaxeHead = new ARItem("CloritePickaxeHead", 1, CreativeTabs.tabMaterials);
+    public static ARItem cloriteSwordHead = new ARItem("CloriteSwordHead", 1, CreativeTabs.tabMaterials);
+    public static ARItem cloriteAxeHead = new ARItem("CloriteAxeHead", 1, CreativeTabs.tabMaterials);
+    public static ARItem cloriteShovelHead = new ARItem("CloriteShovelHead", 1, CreativeTabs.tabMaterials);
+    public static ARItem cloriteHoeHead = new ARItem("CloriteHoeHead", 1, CreativeTabs.tabMaterials);
     public static ARPickaxe cloritePickaxe = new ARPickaxe(clorite, "CloritePickaxe");
     public static ARSword cloriteSword = new ARSword(clorite, "CloriteSword");
     public static ARAxe cloriteAxe = new ARAxe(clorite, "CloriteAxe");
@@ -29,6 +37,12 @@ public class ARItems {
     public static void init() {
 
         GameRegistry.registerItem(cloriteIngot, "CloriteIngot");
+        GameRegistry.registerItem(cloritePlate, "CloritePlate");
+        GameRegistry.registerItem(cloritePickaxeHead, "CloritePickaxeHead");
+        GameRegistry.registerItem(cloriteSwordHead, "CloriteSwordHead");
+        GameRegistry.registerItem(cloriteAxeHead, "CloriteAxeHead");
+        GameRegistry.registerItem(cloriteShovelHead, "CloriteShovelHead");
+        GameRegistry.registerItem(cloriteHoeHead, "CloriteHoeHead");
         GameRegistry.registerItem(cloritePickaxe, "CloritePickaxe");
         GameRegistry.registerItem(cloriteSword, "CloriteSword");
         GameRegistry.registerItem(cloriteAxe, "CloriteAxe");
