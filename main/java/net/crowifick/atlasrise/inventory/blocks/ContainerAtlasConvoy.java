097c15a3ff86620f0a15a6d4dc81c9fb4cf8857e
Crowifick <minecraft4455@live.com>
2014-08-07 20:03:16 -0700

    Fixed Inventory gui render in ContainerAtlasConvoy

--- main/java/net/crowifick/atlasrise/inventory/blocks/ContainerAtlasConvoy.java
+++ main/java/net/crowifick/atlasrise/inventory/blocks/ContainerAtlasConvoy.java
@@ -42,7 +42,7 @@ public class ContainerAtlasConvoy extends Container {
                 addSlotToContainer(new Slot(inventory, j + i * 9 + 9, 8 + j * 18, 83 + i * 18));
 
         for (i = 0; i < 9; ++i)
-            addSlotToContainer(new Slot(inventory, i, 8 + i * 16, 142));
+            addSlotToContainer(new Slot(inventory, i, 8 + i * 18, 142));
 
     }
 
